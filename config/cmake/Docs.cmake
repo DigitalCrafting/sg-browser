2c958f18e513c344bf1091e66c92af49878c76a0
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-06-09 11:59:14 +0200

    [Initial] Structure refactor

--- config/cmake/Docs.cmake
+++ config/cmake/Docs.cmake
@@ -3,7 +3,7 @@ find_package(Doxygen)
 if (DOXYGEN_FOUND)
     add_custom_target(appdocs
             COMMAND ${DOXYGEN_EXECUTABLE}
-            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/docs
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/config/docs
             COMMENT "Generating HTML documentation with Doxygen"
     )
 endif()
