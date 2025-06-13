31c55d463005a70bf014c194d2eefaa54aa0c0cb
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-06-13 16:40:37 +0200

    [Core] CRTP Layer implementation WIP

--- app/sandbox/Sandbox.cpp
+++ app/sandbox/Sandbox.cpp
@@ -1,11 +1,44 @@
 #include "SGBrowser.h"
 
 #include <iostream>
+#include "layers/Layer.h"
+
+
+struct FirstLayer : public SG::Layer<FirstLayer> {
+    void do_onAttach() {
+        std::cout << "First layer onAttach!\n";
+    }
+    
+    void do_onDetach() {
+        std::cout << "First layer onDetach!\n";
+    }
+    
+    void do_onUpdate() {
+        std::cout << "First layer onUpdate!\n";
+    }
+};
+
+struct SecondLayer : public SG::Layer<SecondLayer> {
+    void do_onAttach() {
+        std::cout << "Second layer onAttach!\n";
+    }
+    
+    void do_onDetach() {
+        std::cout << "Second layer onDetach!\n";
+    }
+    
+    void do_onUpdate() {
+        std::cout << "Second layer onUpdate!\n";
+    }
+};
 
 class Sandbox : public SG::Application {
 public:
     Sandbox() {
         std::cout << "Sandbox created!\n";
+        
+        FirstLayer firstLayer;
+        SecondLayer secondLayer;
     }
 };
 
--- src/core/layers/Layer.h
+++ src/core/layers/Layer.h
@@ -5,13 +5,22 @@
 #include "events/Event.h"
 
 namespace SG {
+    template <typename T>
     struct Layer {
         std::string name;
 
-        void onAttach() {};
-        void onDetach() {};
-        void onUpdate() {};
-        void onEvent(Event& event) {};
+        void onAttach() {
+            static_cast<T*>(this)->do_onAttach();
+        };
+        void onDetach() {
+            static_cast<T*>(this)->do_onDetach();
+        };
+        void onUpdate() {
+            static_cast<T*>(this)->do_onUpdate();
+        };
+        void onEvent(Event& event) {
+            static_cast<T*>(this)->do_onEvent(event);
+        };
     };
 }
 
