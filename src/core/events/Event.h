9a6d88e92f25b8ebf9f2b8db027ab91562fd8919
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-06-19 17:27:06 +0200

    [Core] Event enums

--- src/core/events/Event.h
+++ src/core/events/Event.h
@@ -2,10 +2,35 @@
 #define SG_BROWSER_EVENT_H
 
 #include <string>
+#include "../Core.h"
 
 namespace SG {
-    enum EventType {
-
+    enum class EventType {
+        None = 0,
+        WindowClose,
+        WindowResize,
+        WindowFocus,
+        WindowLostFocus,
+        WindowMoved,
+        AppTick,
+        AppUpdate,
+        AppRender,
+        KeyPressed,
+        KeyReleased,
+        KeyTyped,
+        MouseButtonPressed,
+        MouseButtonReleased,
+        MouseMoved,
+        MouseScrolled
+    };
+    
+    enum EventCategory {
+        None = 0,
+        Application = BIT(0),
+        Input = BIT(1),
+        Keyboard = BIT(2),
+        Mouse = BIT(3),
+        MouseButton = BIT(4)
     };
 
     struct Event {
